fe31accd72c69308785f57e027a9ab7f30a9f937
Julia Fehr <09jwithers@gmail.com>
2019-12-01 22:15:09 -0500

    Initial commit. Wireframe of quiz.

---
+++ questions.js
@@ -0,0 +1,40 @@
+const STORE=[
+    {
+        question: "Who was not a Stark?",
+        selections: [
+            "Arya",
+            "Jon",
+            "Sansa",
+            "Theon"],
+        answer: "Theon"},
+    {
+        question: "What was the name of Jon's direwolf?",
+        selections: [
+            "Summer",
+            "Shaggydog",
+            "Nymeria",
+            "Ghost"],
+        answer: "Ghost"},
+    {
+        question: "How did Daenerys' eggs hatch? When they were placed in: ",
+        selections: [
+            "A funeral pyre",
+            "An oven",
+            "A hearth",
+            "A campfire"],
+        answer: "A funeral pyre"},
+    {
+        question: "Who did Sansa not marry?",
+        selections: [
+            "Tyrion",
+            "Little Finger",
+            "Ramsey Bolton"],
+        answer: "Little Finger"},
+    {
+        question: "Which is not one of Daenerys' dragons names?",
+        selections: [
+            "Drogon",
+            "Rhaegal",
+            "Viserion",
+            "Vhagar"],
+        answer: "Vhagar"}];
