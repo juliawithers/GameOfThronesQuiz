e61e0ee45d9b71cf19e55a7aeae55478fa29a636
Julia Fehr <09jwithers@gmail.com>
2019-12-07 17:39:38 -0500

    Possibly the last commit "The God of Death/The Many-Faced God"

--- index.js
+++ index.js
@@ -1,127 +1,146 @@
-// Initialize Load Screen
-// Must contain button that, when clicked, initiates the quiz. 
+
 let score = 0;
 let curIndex = 0;
 
+// Initialize Load Screen:
+// Must contain button that, when clicked, initiates the quiz. 
 function beginQuiz(){
     $('.start-quiz').on('click',function(event){
+        event.preventDefault();
         createQuestion();
-        console.log(`beginQuiz initiated`);
+        // console.log(`beginQuiz initiated`);
     });
 }
-//Quiz Page 
-// Only need to change Section text below <header>
-// When loaded, the questionx/total and score
+//Quiz Page:Only need to change Section text below <header>
+// load and update the count and score
+function updateScore(){
+    const scoreAndCount = 
+    `<p class="question-count">Question number: ${curIndex+1}/${STORE.length}</p>
+    <p class="score">Current Score: ${score}/${STORE.length}</p>`;
+    $('.scoreUpdate').html(scoreAndCount);
+} 
+
 // When loaded, the first question should appear 
 function createQuestion(){
-    console.log(curIndex)
-    console.log('createQuestion ran')
     let question = STORE[curIndex].question;
-    console.log(STORE[curIndex].quesiton);    
-    const scoreAndCount = $(
-    `<p class="question-count">${curIndex}/${STORE.length}</p>
-    <p class="score">${score}/${STORE.length}</p>`)
+    console.log(question);    
+    updateScore(); //call to load the initial score
     const questionHTML = 
-    $(
     `<form>
     <fieldset>
-    <h1>${question}</h1>
-
-    <div class = "selections">
-
-    </div>
+        <h1>${question}</h1>
+        <div class = "options">
+        </div>
     </fieldset>
-    <button class="submit" type = "submit">Submit Answer</button>
-    <button class="next" type = "next">Next Question</button>
-    </form>`
-    );
-    $('.header').append(scoreAndCount);
+    <button class="submit" type = "submit">SUBMIT ANSWER</button>
+    <button class="next" type = "submit">NEXT</button>
+    </form>`;
+
     $('section').html(questionHTML);
     $('.next').hide()
     $('.start-quiz').hide();
     createSelections();
 }
-
+// loads the selctions(input type radio)
 function createSelections(){
     let currentSelections = STORE[curIndex].selections;
     for (let i=0; i<currentSelections.length;i++){
-    $(".selections").append(`<input class="radio" type="radio" name = "selections" id="selections${i}">
-    <label for ="selection${i}">${currentSelections[i]}</label><br>
-    <span class= "selections${i}></span>`)
+    $(".options").append(`<input class = "radio-input" type="radio" name = "selections" value = "${currentSelections[i]}" >
+    <label class = "radio" for ="selections${i}">${currentSelections[i]}</label><br>
+    <span id = "selections${i}"></span>`);
     }
-    console.log('createSelections ran');
-    answerSubmit();
+    questionCheck();
 }
-// Should be able to submit the question
+// Should be able to submit the question. lock the radios after submitting
 // Upon submitting, a box should appear indicating "correct/incorrect" and answer
 // A "next" button should also appear after submittal of the question
 // Clicking "next" should initiate the next quesiton
-function answerSubmit(){
-    $('.submit').on('click', function(){
-        $('.submit').hide();
-        console.log('answerSubmit ran');
-    });
-    questionCheck();
-}
 function questionCheck(){
     $('.submit').on('click',function(event){
-        $('.radio').attr('disabled',true);
-        let currentSelection = $("input[name=selections]:checked").val();
-        let question = $(STORE.question[curIndex]);    
+        $('.submit').hide();
+        $('.next').show();
+        $('.radio-input').prop('disabled', true);
+        event.preventDefault();
+        console.log(curIndex)
+        let currentSelection = $('input[name="selections"]:checked').val();
+
         if (!currentSelection){
             alert('All men must answer.')
         }
-        let answerId = question.selections.findIndex(i=> i === currentSelection);
-        if (currentSelection === question.answer){
-            // correct. find id of selected
-            $('.selections'+answerId).append(`<p>That is correct!</p>`);
+        // answerId is the location of the selected answer
+        let answerId = STORE[curIndex].selections.findIndex(i=> i === currentSelection);
+        // create a variable for the ID of the selected answer span element
+        let id = '#selections'+answerId;
+        if (currentSelection === STORE[curIndex].answer){
+            // correct
+            $(`${id}`).append(`That is correct!<br>`);
+            $(`${id}`).addClass('correctAnswer'); //to be able to edit the boxes in css
+            console.log('.selections'+answerId)
             score++;
+            updateScore();
+            curIndex++;
         }
-        if (currentSelection !== question.answer){
-            // incorrect
-            $('.selections'+answerId).append(`<p>That is not correct. The correct answer is ${STORE[curIndex].answer}</p>`);
+        else{
+        // incorrect
+            console.log('incorrect')
+            $(`${id}`).append(`That is not correct. The correct answer is ${STORE[curIndex].answer}<br>`);
+            $(`${id}`).addClass('wrongAnswer');//to be able to edit the boxes in css
             score += 0;
+            updateScore();
+            curIndex++;
+            
         }
     });
-    curIndex++;
+    
+    loadNextQuestion();
+    // if we have reached the last question, the final page should load instead of the next question
     if(curIndex === STORE.length){
         loadFinalPage();
     }
-    loadNextQuestion();
-    $('.next').show();
-    console.log('questionCheck ran');
 }
 // load the next question when "next" is clicked
 function loadNextQuestion(){
-    $('.next').on('click',function(){
+    $('.next').on('click',function(event){
+        event.preventDefault();
+        if(curIndex === STORE.length){
+            loadFinalPage();
+            return true;
+        }
+        else{
         createQuestion();
         $('next').hide();
-        console.log('loadNextQuestion ran');
+        }
     });
 }
-// Final Page
-// Load the final score and nice message
+// Final Page:
+// Load the final score and message
 function loadFinalPage(){
-
+    saying = phraseOut();
+    $('.scoreUpdate').html(saying);
     const finalPageHTML = 
-    $(
     `<p>Your Score is ${score}/${STORE.length}</p>
-    <button class ="restart">Restart Quiz?</button>`
-    )
+    <button class ="restart">RESTART QUIZ</button>`;
     $('section').html(finalPageHTML);
     curIndex = 0;
     score = 0;
-    $('.restart').on('click',function(){
+    // Restart Quiz? button should be there bringing you back to the beginning of the quiz
+    $('.restart').on('click',function(event){
+        event.preventDefault();
         createQuestion()});
         console.log('loadFinalPage ran');
 }
-// Restart Quiz? button should be there bringing you back to the beginning of the quiz
 
-
-function startThisQuiz(){
-    beginQuiz();
-    // answerSubmit();
-
+// create the message based off of the score
+function phraseOut(){
+    if (score === 0){
+        return 'Valar morghulis. Nice try...';
+    }
+    else if(score === STORE.length){
+        return 'You drink and you know things. Congratulations!';
+    }
+    else{
+        return 'When you play the game of thrones, you win or you die. Try again?';
+    }
 }
 
-$(startThisQuiz);
+$(beginQuiz);
--- questions.js
+++ questions.js
@@ -27,9 +27,9 @@ const STORE=[
         question: "Who did Sansa not marry?",
         selections: [
             "Tyrion",
-            "Little Finger",
+            "Littlefinger",
             "Ramsey Bolton"],
-        answer: "Little Finger"},
+        answer: "Littlefinger"},
     {
         question: "Which is not one of Daenerys' dragons names?",
         selections: [
@@ -37,5 +37,45 @@ const STORE=[
             "Rhaegal",
             "Viserion",
             "Vhagar"],
-        answer: "Vhagar"
-    }];
+        answer: "Vhagar"},
+    {
+        question: "How many actors portrayed The Mountain?"
+        selections:[
+            "One",
+            "Two",
+            "Three",
+            "Four"]
+        answer: "Three"},
+    {
+        question: "What explosive substance was utilized in the Battle of Blackwater Bay?"
+        selections:[
+            "Greek fire",
+            "Wildfire",
+            "Dynamite",
+            "Green fire"]
+        answer: "Wild Fire"},
+    {
+        question: "Besides dragonglass what other material can defeat a White Walker?"
+        selections:[
+            "Wildfire",
+            "Jon Snows piercing gaze",
+            "Valyrian Steel",
+            "Water"]
+        answer: "Valyrian Steel"},
+    {    
+        question: "Which god does Arya briefly serve in Esos?"
+        selections:[
+            "The God of Death/The Many-Faced God",
+            "The Mother",
+            "The Soldier",
+            "The Old Gods"]
+        answer: "The God of Death/The Many-Faced God"},
+    {
+        question: "Who did not help conspire to kill Joffrey Baratheon?"
+        selections:[
+            "Sansa",
+            "Lady Onella",
+            "Littlefinger"]
+        answer: "Sansa"}
+    
+];
